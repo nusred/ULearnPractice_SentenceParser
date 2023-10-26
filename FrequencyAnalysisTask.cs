fed51bfec5bb6dff6aa4ea52f6a05418e02d33ce
nusred <grayterfeed@gmail.com>
2023-10-26 10:22:33 +0300

    added task

---
+++ FrequencyAnalysisTask.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            var result = new Dictionary<string, string>();
+
+            var allGrams = AllNGrams(text);
+            
+            return result;
+        }
+
+        public static Dictionary<string, int> AllNGrams(List<List<string>> words)
+        {
+            var allGrams = new Dictionary<string, int>();
+
+            foreach
+
+            return allGrams;
+        }
+
+        public static 
+   }
+}
---
+++ SentencesParserTask.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+
+            int sentenceCount = 0;
+
+            var sentences = text.Split(new char[] { '.', '!', '?', ';', ':', '(', ')' });
+
+            foreach (var sentence in sentences)
+            {
+                if (sentence.Length != 0)
+                {
+                    foreach (var list in TextSplit(sentence))
+                    {
+                        if (!string.IsNullOrEmpty(list.ToString()))
+                            sentenceCount++;
+
+                        else continue;
+                    }
+
+                    if (sentenceCount != 0)
+                    {
+                        sentencesList.Add(TextSplit(sentence));
+                        sentenceCount = 0;
+                    }
+
+                    else continue;
+                }
+
+                else continue;
+            }
+
+            return sentencesList;
+        }
+
+        public static List<string> TextSplit(string sentence)
+        {
+            var words = new List<string>();
+            var word = new StringBuilder();
+            sentence += ' ';
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (char.IsLetter(sentence[i]) || sentence[i] == '\'')
+                {
+                    word.Append(sentence[i]);
+                }
+
+                else if (!char.IsLetter(sentence[i]) && sentence[i] != '\'' && !string.IsNullOrEmpty(word.ToString()))
+                {
+                    words.Add(word.ToString().ToLower());
+                    word.Remove(0, word.Length);
+                    word.Clear();
+                }
+
+
+                else
+                    continue;
+            }
+
+            return words;
+        }
+    }
+}
